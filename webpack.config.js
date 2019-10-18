82be6e6b0a6b99754ebad397317762de2a830d54
aviboot92 <avkarra@gmail.com>
2019-10-18 13:41:24 +0530

    sass,css,style loader injected into webPack

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,20 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
+        rules: [
+            {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        },{
+            test: /\.scss$/,
+            use:[
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }
+    ]
     },
     devtool: "cheap-module-eval-source-map",
     devServer: {
