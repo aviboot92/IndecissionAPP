f213ae83a83106b2ec5d042e92961d92aa371ea3
aviboot92 <avkarra@gmail.com>
2019-10-21 18:36:55 +0530

    Final working App

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from './components/IndecissionApp';
 import 'normalize.css/normalize.css';
-import './styles/style.scss'
+import './styles/style.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
