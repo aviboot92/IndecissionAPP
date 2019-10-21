f3de2741b0b62adb7e06f2b3ebbbb77ea08d324d
aviboot92 <avkarra@gmail.com>
2019-10-21 14:41:04 +0530

    React Modal is styled

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,11 @@ const OptionModal = (props) =>(
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleModalButton}
         contentLabel="Selected Option"
+        className="modal"
     >
-        <h2>Selected Option: </h2>
-        {props.selectedOption && <h2><i>{props.selectedOption}</i></h2>}
-        <button onClick={props.handleModalButton}>Okay!</button>
+        <h2 className='modal__title'>Selected Option: </h2>
+        {props.selectedOption && <p className='modal__body'><i>{props.selectedOption}</i></p>}
+        <button className='button' onClick={props.handleModalButton}>Okay!</button>
     </Modal>
 );
 
